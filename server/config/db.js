d88bbca99011581b80c4aaf62e94faa23e4484ba
Patience Kamayonza <amanyapatience334@gmail.com>
2026-06-05 20:35:08 +0300

    Feat: restructure the entire .env file

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
+console.log("DATABASE_URL:");
+console.log(process.env.DATABASE_URL);
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
